aafbbb19617a63f904b01cfde6ff3162c20f5f83
squamuglia <maxsmouha@gmail.com>
2018-05-30 18:12:26 -0400

    fixed broken branch, idk how

--- app/rocket_starter.rb
+++ app/rocket_starter.rb
@@ -3,7 +3,8 @@ require 'httparty'
 require 'pry'
 # require 'sinatra/activerecord'
 require_relative 'models/launch.rb'
-
+require_relative 'models/mission.rb'
+require_relative 'models/location.rb'
 
 
 url = 'https://launchlibrary.net/1.4/launch/next/100'
@@ -12,11 +13,12 @@ parsed_response = resp.parsed_response
 
 def launch_seeder(parsed_response)
   parsed_response["launches"].each do |launch|
+    # binding.pry
     Launch.create(name: launch["name"], status: launch["status"], isostart: launch["isostart"], launch_id: launch["id"])
 
-    launch["missions"].each do |mission|
-      Mission.create(name: mission["name"], description: mission["description"], typeName: mission["typeName"], mission_id: mission["id"])
-    end
+     launch["missions"].each do |mission|
+       Mission.create(name: mission["name"], description: mission["description"], typeName: mission["typeName"], mission_id: mission["id"])
+     end
 
     Location.create(name: launch["location"]["name"], latitude: launch["location"]["pads"][0]["latitude"], longitude: launch["location"]["pads"][0]["longitude"], country_code: launch["location"]["country_code"], location_id: launch["location"]["id"])
   end
--- config/environment.rb
+++ config/environment.rb
@@ -3,12 +3,20 @@ require "sinatra/activerecord"
 
 Bundler.require
 
-Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
+# Dir[File.join(File.dirname(__FILE__), "../app/models", "*.rb")].each {|f| require f}
 
 require_relative '../app/menu'
-require_relative '../app/seed.rb'
 require_relative '../app/account.rb'
-require_relative '../app/launch.rb'
+require_relative '../app/launch_methods.rb'
+require_relative '../app/models/mission.rb'
+require_relative '../app/models/launch.rb'
+require_relative '../app/models/user_launch.rb'
+require_relative '../app/models/location.rb'
+require_relative '../app/models/user.rb'
+
+
+
+# require_relative '../app/rocket_starter.rb'
 
 
 
