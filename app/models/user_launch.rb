07eb43687ea73dd9fbeba229511728f02b77f598
JohnZBurnett <tuninator@gmail.com>
2018-05-31 15:27:16 -0400

    write userlaunch date refresher

--- app/models/user_launch.rb
+++ app/models/user_launch.rb
@@ -1,4 +1,8 @@
 class UserLaunch < ActiveRecord::Base
   belongs_to :user
   belongs_to :launch
+
+  def self.date_refresher
+    UserLaunch.all.each {|userlaunch| userlaunch.launch ? true : userlaunch.destroy}
+  end
 end
